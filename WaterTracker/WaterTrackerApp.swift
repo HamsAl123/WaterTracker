f9a16f08ace950ad2910cce636047fb8ed650c0a
Hams Alzahrani <hamsmohsen692@gmail.com>
2023-10-05 12:00:06 +0300

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Hams Alzahrani on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
