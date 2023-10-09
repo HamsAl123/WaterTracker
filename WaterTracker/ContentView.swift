88483a032ff737593b62f8e26be70111da8f4d9c
Hams Alzahrani <hamsmohsen692@gmail.com>
2023-10-09 10:10:21 +0300

    changed some of the ui properities

--- WaterTracker/ContentView.swift
+++ WaterTracker/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var IsOff = true
+    @State private var cups = 0
+    let appTitle="Water Tracker 💦"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(appTitle).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .bold(true)
+                
+                
+            Toggle(isOn: $IsOff) {
+                Text("Apple Health")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                }
+            Stepper(value: $cups, in: 0...20) {
+                Text("Cups to drink per day \(cups)")
+
+            }
+            .padding(.all)
+            Button("Continue") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
